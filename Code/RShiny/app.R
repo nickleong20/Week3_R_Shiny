69d185d1ca56982fa2205fdc8365016505c06054
nickleong20 <nick@mooreplasticresearch.org>
2023-07-07 10:52:37 -0700

    finalized code

--- Code/RShiny/app.R
+++ Code/RShiny/app.R
@@ -3,11 +3,13 @@ if (!require("shiny")) install.packages("shiny")
 if (!require("OpenSpecy")) install.packages("OpenSpecy")
 if (!require("dplyr")) install.packages("dplyr")
 if (!require("DT")) install.packages("DT")
+if (!require("progress")) install.packages("progress")
 
 library(shiny)
 library(OpenSpecy)
 library(dplyr)
 library(DT)
+library(progress)
 
 # Fetch FTIR library
 get_lib()
@@ -32,101 +34,138 @@ ui <- fluidPage(
                   ".0"
                 )
       ),
-      actionButton("analyze", "Analyze")
+      tags$small("Accepted file types: CSV, ASP, JDX, SPC, SPA, 0")
     ),
     mainPanel(
-      DT::dataTableOutput("table"),
-      br(),
-      h4("Result:"),
-      verbatimTextOutput("result")
+      tabsetPanel(
+        tabPanel("Results Table", DT::dataTableOutput("table"))
+      )
     )
   )
 )
 
 # Define server logic
 server <- function(input, output) {
-  data <- reactive({
-    req(input$file)
-    file_extension <- tools::file_ext(input$file$name)
-    if (file_extension %in% c("csv", "CSV")) {
-      read.csv(input$file$datapath, header = TRUE, stringsAsFactors = FALSE)
-    } else if (file_extension %in% c("asp", "ASP")) {
-      OpenSpecy::read_asp(input$file$datapath)
-    } else if (file_extension %in% c("jdx", "JDX")) {
-      OpenSpecy::read_jdx(input$file$datapath)
-    } else if (file_extension %in% c("spc", "SPC")) {
-      OpenSpecy::read_spc(input$file$datapath)
-    } else if (file_extension %in% c("spa", "SPA")) {
-      OpenSpecy::read_spa(input$file$datapath)
-    } else if (file_extension %in% c("0")) {
-      OpenSpecy::read_0(input$file$datapath)
-    } else {
-      # File type not supported
-      stop(paste0("Unsupported file type: ", file_extension))
-    }
-  })
+  data <- reactiveVal(NULL)
   
-  observeEvent(input$analyze, {
-    req(input$file)
-    spec_data <- data()
+  observeEvent(input$file, {
+    progress <- Progress$new(session = shiny::getDefaultReactiveDomain(), min = 0, max = 100)
+    progress$set(message = 'Processing data...')
+    
+    spec_data <- reactive({
+      req(input$file)
+      file_extension <- tools::file_ext(input$file$name)
+      if (file_extension %in% c("csv", "CSV")) {
+        read.csv(input$file$datapath, header = TRUE, stringsAsFactors = FALSE)
+      } else if (file_extension %in% c("asp", "ASP")) {
+        OpenSpecy::read_asp(input$file$datapath)
+      } else if (file_extension %in% c("jdx", "JDX")) {
+        OpenSpecy::read_jdx(input$file$datapath)
+      } else if (file_extension %in% c("spc", "SPC")) {
+        OpenSpecy::read_spc(input$file$datapath)
+      } else if (file_extension %in% c("spa", "SPA")) {
+        OpenSpecy::read_spa(input$file$datapath)
+      } else if (file_extension %in% c("0")) {
+        OpenSpecy::read_0(input$file$datapath)
+      } else {
+        # File type not supported
+        stop(paste0("Unsupported file type: ", file_extension))
+      }
+    })()
     
     # Adjust spectral intensity
     adj_data <- spec_data %>% adj_intens()
+    progress$inc(10, detail = "Adjusted spectral intensity")
     
-    # Smooth and background-correct spectrum
-    proc_data <- adj_data %>% smooth_intens() %>% subtr_bg()
+    # Test if Raman or FTIR 
+    testraman <- match_spec(spec_data, library = spec_lib, which = "raman")
+    testftir <- match_spec(spec_data, library = spec_lib, which = "ftir")
     
-    # Match spectrum with library and retrieve meta data
-    match_result_raman <- match_spec(proc_data, library = spec_lib, which = "raman")
-    match_result_ftir <- match_spec(proc_data, library = spec_lib, which = "ftir")
+    testraman <- head(testraman, 1)
+    testftir <- head(testftir, 1)
     
-    # Determine the type of spectra based on the highest "r" value
-    if (!is.null(match_result_raman) && !is.null(match_result_ftir)) {
-      max_r_raman <- max(match_result_raman$r)
-      max_r_ftir <- max(match_result_ftir$r)
-      
-      if (max_r_raman > max_r_ftir) {
-        match_result <- match_result_raman
+    if (!is.null(testraman) && !is.null(testftir)) {
+      if (testraman$rsq > testftir$rsq) {
         spectrum_type <- "Raman"
       } else {
-        match_result <- match_result_ftir
         spectrum_type <- "FTIR"
       }
-    } else if (!is.null(match_result_raman)) {
-      match_result <- match_result_raman
+    } else if (!is.null(testraman)) {
       spectrum_type <- "Raman"
-    } else if (!is.null(match_result_ftir)) {
-      match_result <- match_result_ftir
+    } else if (!is.null(testftir)) {
       spectrum_type <- "FTIR"
     } else {
-      match_result <- NULL
       spectrum_type <- "Unknown"
     }
+    progress$inc(10, detail = "Identified spectrum type")
     
-    # Check if data was processed or unprocessed
-    if (is.null(match_result)) {
-      data_status <- "Unprocessed"
-    } else {
-      data_status <- "Processed"
+    # Create a vector of smoothing factors
+    smoothing_factors <- 1:7
+    
+    # Create a vector of subtr_bg values
+    subtr_bg_values <- 1:20
+    
+    # Initialize an empty data frame to store the results
+    results <- data.frame(
+      smooth_intens = integer(),
+      subtr_bg = integer(),
+      top_result = character(),
+      type = character(),
+      stringsAsFactors = FALSE
+    )
+    
+    for (factor in smoothing_factors) {
+      for (subtr_bg_value in subtr_bg_values) {
+        # Smooth and background-correct spectrum with current smoothing factor and subtr_bg value
+        proc_data <- adj_data %>% smooth_intens(factor) %>% subtr_bg(subtr_bg_value)
+        
+        if (spectrum_type == "Raman") {
+          # Match spectrum with library and retrieve meta data for Raman
+          match_result_raman <- match_spec(proc_data, library = spec_lib, which = "raman")
+          
+          # Retrieve the top displayed result for Raman
+          top_raman <- head(match_result_raman, 1)
+          
+          # Create data frames for each smoothing factor and subtr_bg value
+          results_raman <- data.frame(
+            smooth_intens = factor,
+            subtr_bg = subtr_bg_value,
+            top_result = top_raman,
+            type = "Raman",
+            stringsAsFactors = FALSE
+          )
+          
+          # Append the data frames to the results data frame
+          results <- rbind(results, results_raman)
+        } else if (spectrum_type == "FTIR") {
+          # Match spectrum with library and retrieve meta data for FTIR
+          match_result_ftir <- match_spec(proc_data, library = spec_lib, which = "ftir")
+          
+          # Retrieve the top displayed result for FTIR
+          top_ftir <- head(match_result_ftir, 1)
+          
+          # Create data frames for each smoothing factor and subtr_bg value
+          results_ftir <- data.frame(
+            smooth_intens = factor,
+            subtr_bg = subtr_bg_value,
+            top_result = top_ftir,
+            type = "FTIR",
+            stringsAsFactors = FALSE
+          )
+          
+          # Append the data frames to the results data frame
+          results <- rbind(results, results_ftir)
+        }
+      }
     }
+    progress$inc(70, detail = "Processed all combinations")
+    progress$close()
     
-    # Check if the region selected was full spectrum or peaks
-    region_status <- ifelse(length(unique(match_result$Region)) > 1, "Peaks", "Full Spectrum")
-    
-    # Print the matched spectrum
-    print("Matched Spectrum:")
-    print(match_result)
-    
-    # Update the data table
+    # Update the results table
     output$table <- DT::renderDataTable({
-      match_result
-    })
-    
-    # Update the result text output
-    output$result <- renderText({
-      paste("Spectrum Type:", spectrum_type, "\n",
-            "Data Status:", data_status, "\n",
-            "Region Status:", region_status)
+      DT::datatable(results, 
+                    colnames = c("Smoothing Intensity", "Baseline Correction", "Sample Name", 
+                                 "Spectrum Identity", "R-Value", "Organization", "Spectrum Type"))
     })
   })
 }
